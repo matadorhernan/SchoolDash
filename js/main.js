edd8d32e158325e212224dfc19bbbf58666cc060
Miguel Romero <miguelesvida@gmail.com>
2019-02-14 09:09:56 -0600

    best 2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(document).ready(function () {
 
     var activeBar = true;
+    var Logged = false;
+
 
     $(".side-nav-controller").click(function () {
         if (activeBar) {
             $('.content').addClass('minimized');
             $('.side-nav-controller').find("i").addClass('fa-rotate-90');
-            activeBar =  false;
+            activeBar = false;
         } else {
             $('.content').removeClass('minimized');
             $('.side-nav-controller').find("i").removeClass('fa-rotate-90');
             activeBar = true;
         }
-        
+
     });
 
+    $(".login-school-controller").click(function () {
+        if (Logged) {
+            $('.app').
+            $('.login').addClass('not-logged');
+            $('.login').removeClass('logged');
+            Logged = false;
+        } else {
+            $('.login').addClass('logged');
+            $('.login').removeClass('not-logged');
+            $('.app').removeClass('no-app');
+            Logged = true;
+        }
+
+    });
+
+  
 });
